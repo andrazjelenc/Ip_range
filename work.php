4b34a58439c0de56ff1633a5b19b96f59d6ec1dd
andrazjelenc <andraz.jelenc@siol.net>
2015-09-27 18:16:48 +0200

    Update work.php
    Zoptimizirano

--- work.php
+++ work.php
@@ -1,108 +1,43 @@
 <?php
 
-function inRange($ip, $range)
-{
-	$range = explode('-',$range);
-	$startRange = ip2long($range[0]);
-	$stopRange = ip2long($range[1]);
-	
-	$ip = ip2long($ip);
-	
-	if($ip >= $startRange-1 && $ip <= $stopRange+1)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-	
-}
-
-function repairRange($rangeA, $rangeB)
-{
-	$razbitRangeB = explode('-', $rangeB);
-	$startIpB = $razbitRangeB[0];
-	$stopIpB = $razbitRangeB[1];
-
-	
-	if(inRange($startIpB, $rangeA) && inRange($stopIpB, $rangeA))
-	{
-		return $rangeA; // a je znotraj b-ja
-	}
-	elseif(inRange($startIpB, $rangeA))
-	{
-		//A-D
-		$razbitRangeA = explode('-', $rangeA);
-		$startIpA = $razbitRangeA[0];
-		$stopIpA = $razbitRangeA[1];
-		return $startIpA.'-'.$stopIpB;
-	}
-	elseif(inRange($stopIpB, $rangeA))
-	{
-		//C-B
-		$razbitRangeA = explode('-', $rangeA);
-		$startIpA = $razbitRangeA[0];
-		$stopIpA = $razbitRangeA[1];
-		return $startIpB.'-'.$stopIpA;
-	}
-	else
-	{
-		return 'new';
-	}
-}
-
-$output = array();
+$lastStartIp = '';
+$lastStopIp = '';
 
 $content = file_get_contents('list.txt');
 $content = explode("\n",$content);
 
-foreach($content as $line)
+$firstIp = explode('-', $content[0]);
+$lastStartIp = $firstIp[0];
+$lastStopIp = $firstIp[1];
+unset($content[0]);
+
+foreach($content as $range)
 {
-	$range = trim($line);
 	if(empty($range))
 	{
 		continue;
 	}
-
-	if(count($output) == 0)
-	{
-		$output[] = $range;
-		continue;
-	}
 		
-	//echo $range."\r\n";
-	$inserted = false;
+	$lastStopIp2long = ip2long($lastStopIp);
 	
-	$startIndex = count($output) - 50;
-	if($startIndex < 0)
-	{
-		$startIndex = 0;
-	}
+	$newRange = explode('-',$range);
 	
-	for($i = $startIndex; $i < count($output); $i++)
+	$newStartIp = $newRange[0];
+	$newStartIp2long = ip2long($newStartIp);
+	
+	$newStopIp = $newRange[1];
+	
+	if($newStartIp2long <= $lastStopIp2long + 1) //prekrivane ali dotikanje
 	{
-		$rangeB = $output[$i];
-		//echo '--->'.$rangeB;
-		$result = repairRange($range,$rangeB);
-		if(!($result == 'new'))
-		{
-			$output[$i] = $result;
-			$inserted = true;
-			//echo " [INSERTED]\r\n";
-			break;
-		}
-		//echo "\r\n";
-		
+		$lastStopIp = $newStopIp;
 	}
-	if(!$inserted)
+	else
 	{
-		$output[] = $range;
+		echo $lastStartIp.'-'.$lastStopIp."\n";
+		$lastStartIp = $newStartIp;
+		$lastStopIp = $newStopIp;
 	}
 }
+echo $lastStartIp.'-'.$lastStopIp;
 
-foreach($output as $range)
-{
-	echo $range."\n";
-}
 ?>
